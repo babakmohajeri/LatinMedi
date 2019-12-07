e8e14cdba9829e32194fc4625066ad3c02a20b9d
babakmohajeri <babak.mohajeri@gmail.com>
2019-12-07 23:07:27 +0330

    Test2

---
+++ LatinMedi.Core/Convertor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatinMedia.Core.Convertor
+{
+    class Class1
+    {
+        public int sss { get; set; }
+
+    }
+}
---
+++ LatinMedia.DataLayer/Context/LatinMediaDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LatinMedia.DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LatinMedia.DataLayer.Context
+{
+    public class LatinMediaDbContext:DbContext
+    {
+        public LatinMediaDbContext(DbContextOptions<LatinMediaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
